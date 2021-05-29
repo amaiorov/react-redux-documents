3238551927745d7c209923f60a69a2bc0a423645
Alexey Maiorov <alexey.maiorov@utc.com>
2021-05-29 18:27:13 -0400

    add actions

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,3 +1,5 @@
+import ADD_ARTICLE from 'js/constants/action-types';
+
 export function addArticle(payload) {
-  return { type: 'ADD_ARTICLE', payload };
+  return { type: ADD_ARTICLE, payload };
 };
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,20 @@
+import { ADD_ARTICLE } from 'js/constants/action-types';
+
 const initialState = {
   articles: []
 };
 
 function rootReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return {
+        ...state,
+        articles: state.articles.concat(action.payload)
+      };
+      break;
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
